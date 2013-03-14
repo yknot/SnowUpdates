1919bf88de7e677144fca1a9d727d29c05099c79
yknot <a.yale9@gmail.com>
2013-03-13 21:29:26 -0400

    added noaa weather for killington peak

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import smtplib
 
-from killington import *
+from killingtonSnow import *
+from killingtonWeather import *
 
 # set up smtp server
 server = smtplib.SMTP( "smtp.gmail.com", 587 )
@@ -12,8 +13,11 @@ password = open("password").read()
 # login to server
 server.login( 'snowupdates@gmail.com', password )
 
-# get the message
-msg = killingtonSnow()
+# get the killington snow report msg
+# msg = killingtonSnow()
+
+# get the killington weather report msg
+msg = killingtonWeather()
 
 # get number from local file
 number = open("number").read()
