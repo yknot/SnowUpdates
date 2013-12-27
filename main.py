23adb3b85b6c3333cca06a636a1cd043fb815e4d
Andrew Yale <a.yale9@gmail.com>
2013-12-27 15:11:02 -0500

    add command line arguments for user

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 import xml.etree.ElementTree as ET
+import sys
 
+# user imports
 from pushBullet import PushBullet
-
 from getMsg import *
 
 # open users xml file to get api_key
 root = ET.parse('users.xml').getroot()
 
-# turn this into a command line arg
-name = 'Andrew Yale'
+if len(sys.argv) > 1:
+	name = str(sys.argv[1])
+else:
+	name = 'Andrew Yale'
+
 # set empy api key
 apiKey = ''
 
@@ -20,11 +24,9 @@ for child in root:
 		apiKey = child.find('api_key').text
 		break
 
-if apiKey == ''
+if apiKey == '':
 	print 'User not found'
 else:
-	# get api key for the user
-	apiKey = open('api_key').read()
 	# make push bullet object with api key
 	p = PushBullet(apiKey)
 	# Get a list of devices
@@ -32,20 +34,20 @@ else:
 
 	# for each of those resorts the user has chosen
 	for child in user_data.find('resorts'):
-		# set the resort value from the user_data
-		resort = child.text
+	    # set the resort value from the user_data
+	    resort = child.text
 
-	    # get section of xml for the right resort
-	    resort_data = getXml(resort)
+        # get section of xml for the right resort
+        resort_data = getXml(resort)
 
-	    # get the soup object of the website
-	    soup = getSoup(resort_data.find('url').text)
+        # get the soup object of the website
+        soup = getSoup(resort_data.find('url').text)
 
-	    # get the table for the snow stats
-	    table = getStatsTable(soup, resort_data)
+        # get the table for the snow stats
+        table = getStatsTable(soup, resort_data)
 
-	    # get the conditions message
-	    msg = getConditionsMsg(table, resort_data)
+        # get the conditions message
+        msg = getConditionsMsg(table, resort_data)
 
     	# push the note to the device
     	p.pushNote(devices[0]['id'], resort_data.find('name').text + ' Snow Report', msg)
